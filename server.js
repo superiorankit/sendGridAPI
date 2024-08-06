74f7bdac020dc6620c616621a7dbdc103593f96c
superiorankit <ankit.kumar@techsuperiors.com>
2024-08-06 18:44:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/mailTrack', (req, res) => {
+    console.log('hitted')
+    const events = req.body;
+    if (!events)
+        return res.status(300).send('Status not recieved')
+    try{
+    events.forEach(event => {
+        console.log(`Event: ${event.event}, Email: ${event.email}`);
+    });
+    return res.status(200).send('Status recieved')
+}
+catch(err){
+    return res.status(400).send('Something went wrong')
+}
+});
+
+app.listen(3000, () => {
+    console.log(`Server Started..........`)
+})
